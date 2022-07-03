fc4532ba6b7a8d302516d252480bfa3bb7adfadc
from-apollo <100309059+from-apollo@users.noreply.github.com>
2022-07-03 21:32:56 +0300

    created functions

---
+++ Homework7/Homework7.Exersice1.js
@@ -0,0 +1,53 @@
+function Animal(name) {
+
+    this._foodAmount = 50;
+
+    this.name = name;
+}
+
+Animal.prototype._formatFoodAmount = function() {
+    return this._foodAmount + ' гр.';
+};
+
+Animal.prototype.dailyNorm = function(amount) {
+    if (!arguments.length) return this._formatFoodAmount();
+
+    if (amount < 30 || amount > 100) {
+        return 'Недопустимое количество корма.';
+    }
+
+    this._foodAmount = amount;
+};
+
+Animal.prototype.feed = function() {
+    console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+    return this;
+};
+
+function Cat(name) {
+
+    Animal.apply(this, arguments);
+
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.feed = function() {
+    Animal.prototype.feed.apply(this);
+    console.log('Кот доволен ^_^');
+    return this;
+}
+
+Cat.prototype.stroke = function() {
+    console.log('Гладим кота');
+    return this;
+}
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.dailyNorm(75));
+barsik.feed();
+
+console.log(barsik.stroke().stroke().feed().stroke());
